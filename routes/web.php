2566950e5bd4d2abebb0db4d3d0e28e1c8dbc975
OverlyDevoted <58229165+OverlyDevoted@users.noreply.github.com>
2023-08-26 20:41:34 +0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/hello', function () {
+    return response("Hello world!");
+});
+Route::get('/posts/{id}', function ($id) {
+    //dd($id);
+    return response("Post " . $id);
+})->where('id', '[0-9]+');
+
+Route::get('/search', function (Request $request) {
+    dd($request->name . ' ' . $request->city);
+    return response("Hihg");
+})->where('id', '[0-9]+');
