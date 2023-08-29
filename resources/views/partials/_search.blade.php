f32673b2e08ebf38723049fb628a270e094f0c80
OverlyDevoted <58229165+OverlyDevoted@users.noreply.github.com>
2023-08-29 17:43:45 +0300

    Database migrations, seeding, factories, tailwind and styling

---
+++ resources/views/partials/_search.blade.php
@@ -0,0 +1,9 @@
+<form action="/">
+    <div class="relative mx-8 py-4">
+
+        <input class="border-2 rounded-md px-8 w-full h-10 bg-gray-100 border-gray-200" type="text" name="search"
+            placeholder="Search in Larafolio...">
+        
+        <button class="absolute bg-primary-light text-white rounded-md px-4 py-1 right-1 bottom-5 hover:bg-secondary-light transition-colors duration-300" type="submit">Search</button>
+    </div>
+</form>
